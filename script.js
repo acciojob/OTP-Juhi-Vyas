48c949c7954b86b18e8da2f392db1d59f22d6d0c
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-08 23:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ window.addEventListener("load", () => {
 inputs.forEach((item, index) => {
 
   item.addEventListener("input", () => {
-    if (item.value.length === 1 && index < inputs.length - 1) {
+    if (item.value.length == 1 && index < inputs.length - 1) {
       inputs[index + 1].focus()
     }
   })
 
   item.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && item.value === "" && index > 0) {
+    if (e.key == "Backspace" && item.value == "" && index > 0) {
       inputs[index - 1].focus()
     }
   })
