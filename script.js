241a9cd8732bb53fbe7e048d29f39670e19aed5c
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-08 23:48:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-let input = document.querySelectorAll(".code")
-let p = document.querySelector("#para")
-let i = 0
-window.addEventListener("load",()=>{
-	input[0].focus()
+let inputs = document.querySelectorAll(".code")
+
+window.addEventListener("load", () => {
+  inputs[0].focus()
 })
 
-input.forEach((item) => {
-	item.addEventListener("input",()=>{
-		if(item.value.length == 1 && i < 6){
-		input[++i].focus();
-			p.textContent = i
-		}
-	})
+inputs.forEach((item, index) => {
+
+  item.addEventListener("input", () => {
+    if (item.value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus()
+    }
+  })
+
+  item.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && item.value === "" && index > 0) {
+      inputs[index - 1].focus()
+    }
+  })
+
 })
-
-input.forEach((item) => {
-	item.addEventListener("keydown",(event)=>{
-	if(event.key == "Backspace"){
-		item.value = ""
-		input[--i].focus();
-		p.textContent = i
-		}
-	})
-})
-
-
-
-
-
-
